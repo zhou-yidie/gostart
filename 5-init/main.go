9185b7e53190a0e3ed8732d19cf94e592ccb5b8f
zhou-yidie <2261328488@qq.com>
2024-12-20 10:58:55 +0800

    go_init

---
+++ 5-init/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "start/5-init/lib1"
+	mylib2 "start/5-init/lib2"
+	//. "start/5-init/lib2"
+)
+
+func main() {
+	//lib1.Lib1Test()
+
+	//lib2.Lib1Test()
+	mylib2.Lib1Test()
+	//Lib1Test()
+}
