ed7c93a4b936d2ebaceafaf8e764921351492a33
zhou-yidie <2261328488@qq.com>
2025-01-09 16:25:34 +0800

    lock

---
+++ ch12/lock/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+var total int32
+var wg sync.WaitGroup
+
+//var lock sync.Mutex
+
+func add() {
+	defer wg.Done()
+	for i := 0; i < 5000000; i++ {
+		atomic.AddInt32(&total, 1)
+		// lock.Lock()
+		// total++
+		// lock.Unlock()
+	}
+}
+
+func sub() {
+	defer wg.Done()
+	for i := 0; i < 5000000; i++ {
+		atomic.AddInt32(&total, -1)
+		// lock.Lock()
+		// total--
+		// lock.Unlock()
+	}
+}
+
+func main() {
+	wg.Add(2)
+	go add()
+	go sub()
+	wg.Wait()
+	fmt.Println(total)
+}
